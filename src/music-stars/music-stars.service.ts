7338f02e0d5017439f4797ae42c0158fc76a4bb5
Morritz <12800230+Morritz@users.noreply.github.com>
2022-06-03 00:21:49 +0200

    data loading for musicstarsservice & getAll handled

--- src/music-stars/music-stars.service.ts
+++ src/music-stars/music-stars.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { readFileSync } from 'fs';
 import { MusicStar } from './entities/music-star.entity';
 
 @Injectable()
-export class MusicStarsService {
+export class MusicStarsService implements OnModuleInit {
+  private musicStars: MusicStar[];
+
+  onModuleInit() {
+    try {
+      const data = readFileSync('./data/musicStars.json', 'utf-8');
+      const parsedData = JSON.parse(data);
+      this.musicStars = parsedData;
+    } catch (e) {
+      console.log(e);
+      this.musicStars = [];
+    }
+  }
+
   getAllMusicStars() {
-    return;
+    return this.musicStars;
   }
   addNewMusicStar() {
     return;
