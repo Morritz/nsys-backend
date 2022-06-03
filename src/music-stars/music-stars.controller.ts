710e03c51810c9b2538046103cd3d247e2f5903c
Morritz <12800230+Morritz@users.noreply.github.com>
2022-06-03 12:16:36 +0200

    trim pipe

--- src/music-stars/music-stars.controller.ts
+++ src/music-stars/music-stars.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  PipeTransform,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { AddMusicStarDto } from './dto/add-music-star.dto';
 import { MusicStarsService } from './music-stars.service';
 
@@ -11,6 +20,19 @@ export class MusicStarsController {
   }
 
   @Post()
+  @UsePipes(
+    new (class implements PipeTransform {
+      transform(value: any, metadata: ArgumentMetadata) {
+        if (typeof value === 'object') {
+          Object.keys(value).forEach((key) => {
+            if (typeof value[key] === 'string') value[key] = value[key].trim();
+          });
+          return value;
+        }
+        throw new BadRequestException('Malformed body.');
+      }
+    })(),
+  )
   addNewMusicStar(@Body() addMusicStarDto: AddMusicStarDto) {
     this.musicStarsService.addNewMusicStar(addMusicStarDto);
   }
